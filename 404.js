cc8d194cfa135bd816e8ce7d3e6693d10ffc2a14
Jacob Waters <jpwaters09@gmail.com>
2025-05-10 19:11:26 +0100

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,19 @@
+function home() {
+    window.location.href = "https://jpwaters09.github.io";
+}
+
+function homeLink() {
+    window.location.href = "https://jpwaters09.github.io";
+}
+
+function projectsLink() {
+    window.location.href = "https://jpwaters09.github.io/projects";
+}
+
+function contactLink() {
+    window.location.href = "https://jpwaters09.github.io/contactme";
+}
+
+function aboutLink() {
+    window.location.href = "https://jpwaters09.github.io/about";
+}
