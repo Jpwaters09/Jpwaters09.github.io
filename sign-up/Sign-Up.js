9d254f73778403b3f4f7e90a0394c61532cf6d97
Jacob Waters <jpwaters09@gmail.com>
2025-10-30 18:27:29 +0000

    Updated

---
+++ sign-up/Sign-Up.js
@@ -0,0 +1,65 @@
+const SUPABASE_URL = "https://yuppojdiwoxipbxprhdg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1cHBvamRpd294aXBieHByaGRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NDc2NjgsImV4cCI6MjA3NzMyMzY2OH0.WZOVWaNexQMPK1ZFfMabM7xgWMaB-s0lKJ7GQjkFEK8";
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+function ChangePasswordVisibility() {
+    let signUpPassword = document.getElementById("signUpPassword");
+    let signUpPasswordConfirm = document.getElementById("signUpPasswordConfirm");
+    let passwordEyeImg = document.getElementById("passwordEyeImg");
+
+    if (signUpPassword.type == "password") {
+        signUpPassword.type = "text";
+        signUpPasswordConfirm.type = "text";
+        passwordEyeImg.src = "/assets/icons/Visibility On.svg";
+    }
+
+    else if (signUpPassword.type == "text") {
+        signUpPassword.type = "password";
+        signUpPasswordConfirm.type = "password";
+        passwordEyeImg.src = "/assets/icons/Visibility Off.svg";
+    }
+}
+
+async function signUp(event) {
+    let signUpForm = document.getElementById("signUpForm");
+    let signUpName = document.getElementById("signUpName");
+    let signUpEmail = document.getElementById("signUpEmail")
+    let signUpPassword = document.getElementById("signUpPassword");
+    let signUpPasswordConfirm = document.getElementById("signUpPasswordConfirm");
+    let output = document.getElementById("output");
+    let emailConfirm = document.getElementById("emailConfirm");
+    let emailConfirmSubtitle = document.getElementById("emailConfirmSubtitle");
+
+    if (signUpPassword.value != signUpPasswordConfirm.value) {
+        output.textContent = "Passwords do not match.";
+        event.preventDefault();
+        return;
+    }
+
+    output.textContent = "";
+
+    const {data, error} = await supabase.auth.signUp({email: signUpEmail.value, password: signUpPassword.value});
+
+    if (error) {
+        output.textContent = error;
+        event.preventDefault();
+        alert(error);
+        return;
+    }
+
+    const user = data.user;
+
+    if (!user) {
+        output.textContent = "Check your email to confirm your account.";
+        event.preventDefault();
+        return;
+    }
+
+    await supabase
+        .from("users")
+        .insert([{id: user.id, email: user.email, profile: {name: signUpName.value}}]);
+
+    signUpForm.style.display = "none";
+    emailConfirm.style.display = "";
+    emailConfirmSubtitle.innerHTML = `We have sent a verification link to <b>${signUpEmail.value}</b>`;
+}
